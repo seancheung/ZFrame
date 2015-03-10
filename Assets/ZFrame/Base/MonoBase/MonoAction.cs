d86b0e656ff5f50495d9911d3db2eb7c19f2eb69
Sean <theoxuanx@gmail.com>
2015-03-10 20:30:17 +0800

    MonoAction

---
+++ Assets/ZFrame/Base/MonoBase/MonoAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class MonoAction : ISerializationCallbackReceiver
+{
+    [SerializeField] private Component component;
+
+    private Action action;
+
+    public string method;
+
+    public void OnBeforeSerialize()
+    {
+        if (action != null)
+            method = action.Method.Name;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        if (component && component)
+            action = (Action) Delegate.CreateDelegate(typeof (Action), component, method);
+    }
+
+    public static Delegate CreateDelegate(Type type, MonoAction monoAction)
+    {
+        if (monoAction.component == null || string.IsNullOrEmpty(monoAction.method))
+        {
+            return null;
+        }
+
+        return Delegate.CreateDelegate(type, monoAction.component, monoAction.method, false, false);
+    }
+
+    public static Delegate CreateDelegate<T>(MonoAction monoAction)
+    {
+        return CreateDelegate(typeof(T), monoAction);
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Component: {0}, Method: {1}", component, method);
+    }
+}

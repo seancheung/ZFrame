355158d3779236c541b2f3ce15db1466b2097ac2
Sean <theoxuanx@gmail.com>
2015-05-22 18:34:45 +0800

    Interface component loader

---
+++ Assets/ZFrame/Utilities/MonoInterfaceLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public static class MonoInterfaceLoader
+{
+    public static void Load<T>(this GameObject gameObject) where T : class
+    {
+        foreach (
+            Type type in
+                Assembly.GetExecutingAssembly()
+                    .GetTypes()
+                    .Where(t => !t.IsAbstract && typeof (T).IsAssignableFrom(t)))
+        {
+            gameObject.AddComponent(type);
+        }
+    }
+
+    public static void Load<T>(this Component component) where T : class
+    {
+        Load<T>(component.gameObject);
+    }
+}
